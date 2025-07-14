8d0384a30dfd3678c139078bc9d005912e5d89aa
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2025-07-14 02:00:55 -0300

    footer

--- react/components/Newsletter/index.jsx
+++ react/components/Newsletter/index.jsx
@@ -5,11 +5,9 @@ import axios from 'axios';
 
 import "./style.global.css";
 
-function Newsletter({ title, placeholderemail, placeholdername, textbutton }) {
+function Newsletter({ title, subTitle, placeholderemail, placeholdername, textbutton }) {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
-  const [genero, setGenero] = useState('');
-  const [estado, setEstado] = useState('');
   const [show, setShow] = useState(false);
 
   const validateEmail = (email) => {
@@ -29,17 +27,13 @@ function Newsletter({ title, placeholderemail, placeholdername, textbutton }) {
     }
 
     axios
-      .patch(`/api/dataentities/NW/documents`, {
+      .patch(`/api/dataentities/NL/documents`, {
         nome: name,
         email: email,
-        genero: genero,
-        estado: estado,
       })
       .then(({ data }) => {
         setEmail('');
         setName('');
-        setGenero('');
-        setEstado('');
         alert('Obrigado por se cadastrar em nossa Newsletter!');
       });
   };
@@ -52,8 +46,8 @@ function Newsletter({ title, placeholderemail, placeholdername, textbutton }) {
     <section className="vtex-hide-react">
       <div className="e-newsletter-center">
         <div className="e-newsletter-top">
-          <img alt="Newsletter" src="https://babycalcados.vteximg.com.br/arquivos/newsletter.png" />
           <h2>{title}</h2>
+          <h3>{subTitle}</h3>
         </div>
 
         <form className="e-form-newsletter" onSubmit={handleSubmit}>
@@ -73,42 +67,6 @@ function Newsletter({ title, placeholderemail, placeholdername, textbutton }) {
             name="email"
             placeholder={placeholderemail}
           ></input>
-          <select name="genero" id="genero" required="" onChange={(e) => setGenero(e.target.value)}>
-            <option value="">Gênero</option>
-            <option value="Feminino">Feminino</option>
-            <option value="Masculino">Masculino</option>
-          </select>
-          <select name="estado" id="estado" required="" onChange={(e) => setEstado(e.target.value)}>
-            <option value="">Estado</option>
-            <option value="AC">Acre</option>
-            <option value="AL">Alagoas</option>
-            <option value="AP">Amapá</option>
-            <option value="AM">Amazonas</option>
-            <option value="BA">Bahia</option>
-            <option value="CE">Ceará</option>
-            <option value="DF">Distrito Federal</option>
-            <option value="ES">Espírito Santo</option>
-            <option value="GO">Goiás</option>
-            <option value="MA">Maranhão</option>
-            <option value="MT">Mato Grosso</option>
-            <option value="MS">Mato Grosso do Sul</option>
-            <option value="MG">Minas Gerais</option>
-            <option value="PA">Pará</option>
-            <option value="PB">Paraíba</option>
-            <option value="PR">Paraná</option>
-            <option value="PE">Pernambuco</option>
-            <option value="PI">Piauí</option>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="RN">Rio Grande do Norte</option>
-            <option value="RS">Rio Grande do Sul</option>
-            <option value="RO">Rondônia</option>
-            <option value="RR">Roraima</option>
-            <option value="SC">Santa Catarina</option>
-            <option value="SP">São Paulo</option>
-            <option value="SE">Sergipe</option>
-            <option value="TO">Tocantins</option>
-            <option value="EX">Estrangeiro</option>
-          </select>
           <button type="submit" className="e-btn">
             {textbutton}
           </button>
